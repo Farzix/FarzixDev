701ed2964f1ee58b450576b76e371353ca61483e
Peter Ayvazian <peter.ayvazian@outlook.com>
2015-11-24 11:06:14 +0100

    Created solution, added existing code

---
+++ MVC_Bootstrap_Design/MVC_Bootstrap_Design/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Bootstrap_Design.Startup))]
+namespace MVC_Bootstrap_Design
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
