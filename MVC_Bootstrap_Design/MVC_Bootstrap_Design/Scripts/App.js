98ce8704c953d86797f02770a031bd3525716897
Peter Ayvazian <peter.ayvazian@outlook.com>
2015-11-26 11:25:31 +0100

    fixed stuff

--- MVC_Bootstrap_Design/MVC_Bootstrap_Design/Scripts/App.js
+++ MVC_Bootstrap_Design/MVC_Bootstrap_Design/Scripts/App.js
@@ -1,23 +1,51 @@
-﻿$(document).ready(function () {
-    // Add smooth scrolling to all links in navbar + footer link
-    $(".navbar a, footer a[href='#myPage']").on('click', function (event) {
+﻿'use strict';
 
-        // Prevent default anchor click behavior
-        event.preventDefault();
+//var pathname = window.location.pathname; // Returns path only
+//var url = window.location.href;     // Returns full URL
 
-        // Store hash
-        var hash = this.hash;
+$(document).ready(function () {
+    $('a').on('click', function () {
+        switch (this.id) {
+            case 'navNews':
+                SmoothSlide('#myNews');
+                //scrollTo = '#myNews';
+                break;
+            case 'navProjects':
+                SmoothSlide('#myProjects');
+                //scrollTo = '#myProjects';
+                break;
+            case 'navBlogs':
+                SmoothSlide('#myBlogs');
+                //scrollTo = '#myBlogs';
+                break;
+            default:
+                break;
+        }
 
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 900, function () {
-
-            // Add hash (#) to URL when done scrolling (default click behavior)
-            window.location.hash = hash;
-        });
+        //$('html, body').animate({
+        //    scrollTop: $(scrollTo).offset().top
+        //}, 900);
     });
+    //alert(pathname);
+    // Add smooth scrolling to all links in navbar + footer link
+    //$(".navbar a, footer a[href='#myPage']").on('click', function (event) {
+
+    //    // Prevent default anchor click behavior
+    //    event.preventDefault();
+
+    //    // Store hash
+    //    var hash = this.hash;
+
+    //    // Using jQuery's animate() method to add smooth page scroll
+    //    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    //    $('html, body').animate({
+    //        scrollTop: $(hash).offset().top
+    //    }, 900, function () {
+
+    //        // Add hash (#) to URL when done scrolling (default click behavior)
+    //        window.location.hash = hash;
+    //    });
+    //});
 
     $(window).scroll(function () {
         $(".slideanim").each(function () {
@@ -29,4 +57,10 @@
             }
         });
     });
-})
+});
+
+function SmoothSlide(scrollTo) {
+    $('html, body').animate({
+        scrollTop: $(scrollTo).offset().top
+    }, 900);
+}
